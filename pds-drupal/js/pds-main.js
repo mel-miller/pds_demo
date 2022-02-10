b39f28c4045f808ccad66d354f9a742acdc04009
M Miller <miller.2676@gmail.com>
2022-02-09 17:43:15 -0800

    configure modal block type

--- pds-drupal/js/pds-main.js
+++ pds-drupal/js/pds-main.js
@@ -1,37 +1,44 @@
 Drupal.behaviors.modal = {
 	attach(context) {
-		const openBtn = document.getElementById("openBtn");
-		const closeBtn = document.querySelector(".modal .close-btn");
-		const modal = closeBtn.closest(".modal");
+		const modals = document.querySelectorAll(".modal");
 
-		// Open modal via open button.
-		openBtn.addEventListener("click", (e) => {
-			e.preventDefault();
-			modal.style.display = "block";
-			openBtn.style.display = "none";
+		modals.forEach((modal) => {
+			const modalId = modal.id;
+			const modalNum = modalId.substring(modalId.indexOf("--") + 2);
+
+			const openBtn = document.getElementById(`modalBtn--${modalNum}`);
+			const currentModal = document.getElementById(`modal--${modalNum}`);
+			const closeBtn = currentModal.querySelector(".close-btn");
+
+			// Open modal via open button.
+			openBtn.addEventListener("click", (e) => {
+				e.preventDefault();
+				currentModal.style.display = "block";
+				openBtn.disabled = true;
+			});
+
+			// Close modal via close button.
+			closeBtn.addEventListener("click", (e) => {
+				e.preventDefault();
+				currentModal.style.display = "none";
+				openBtn.disabled = false;
+			});
+
+			// Close modal via "esc" key.
+			window.addEventListener("keydown", function (event) {
+				if (event.key === "Escape") {
+					currentModal.style.display = "none";
+					openBtn.disabled = false;
+				}
+			});
+
+			// Close modal by clicking outside of modal content area.
+			window.onclick = function (e) {
+				if (e.target == modal) {
+					currentModal.style.display = "none";
+					openBtn.disabled = false;
+				}
+			};
 		});
-
-		// Close modal via close button.
-		closeBtn.addEventListener("click", (e) => {
-			e.preventDefault();
-			modal.style.display = "none";
-			openBtn.style.display = "block";
-		});
-
-		// Close modal via "esc" key.
-		window.addEventListener("keydown", function (event) {
-			if (event.key === "Escape") {
-				modal.style.display = "none";
-				openBtn.style.display = "block";
-			}
-		});
-
-		// Close modal by clicking outside of modal content area.
-		window.onclick = function (e) {
-			if (e.target == modal) {
-				modal.style.display = "none";
-				openBtn.style.display = "block";
-			}
-		};
 	},
 };
